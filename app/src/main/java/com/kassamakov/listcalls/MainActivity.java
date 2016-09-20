2261cf503e301d918d264c2dbb9f31bd08463963
Ivailo Kassamakov <ivokassamakov@gmail.com>
2016-09-21 00:58:29 +0200

    Initial import

---
+++ app/src/main/java/com/kassamakov/listcalls/MainActivity.java
@@ -0,0 +1,163 @@
+package com.kassamakov.listcalls;
+
+import android.app.Activity;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.CallLog;
+import android.view.Gravity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends Activity
+        implements OnClickListener {
+    Button btnAll;
+    Button btnIncoming;
+    Button btnOutgoing;
+    Button btnMissed;
+
+    TableLayout tblMain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        try {
+            btnAll = (Button) findViewById(R.id.btnAll);
+            btnAll.setOnClickListener(this);
+
+            btnIncoming = (Button) findViewById(R.id.btnIncoming);
+            btnIncoming.setOnClickListener(this);
+
+            btnMissed = (Button) findViewById(R.id.btnMissed);
+            btnMissed.setOnClickListener(this);
+
+            btnOutgoing = (Button) findViewById(R.id.btnOutgoing);
+            btnOutgoing.setOnClickListener(this);
+
+            tblMain = (TableLayout) findViewById(R.id.tblMain);
+        } catch (Exception ex) {
+            Toast.makeText(this, "Error in MainActivity.onCreate: " + ex.getMessage(),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        Cursor cursor;
+
+        Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
+        try {
+            switch (v.getId()) {
+                case R.id.btnAll:
+                    cursor = this.getContentResolver().query(CallLog.Calls.CONTENT_URI,
+                            null,
+                            null,
+                            null,
+                            CallLog.Calls.DATE + " DESC");
+
+                    CursorToTable(cursor, tblMain);
+                    break;
+
+                case R.id.btnIncoming:
+                    cursor = this.getContentResolver().query(CallLog.Calls.CONTENT_URI,
+                            null,
+                            CallLog.Calls.TYPE + " = " + CallLog.Calls.INCOMING_TYPE,
+                            null,
+                            CallLog.Calls.DATE + " DESC");
+
+                    CursorToTable(cursor, tblMain);
+                    break;
+
+                case R.id.btnOutgoing:
+                    cursor = this.getContentResolver().query(CallLog.Calls.CONTENT_URI,
+                            null,
+                            CallLog.Calls.TYPE + " = " + CallLog.Calls.OUTGOING_TYPE,
+                            null,
+                            CallLog.Calls.DATE + " DESC");
+
+                    CursorToTable(cursor, tblMain);
+                    break;
+
+                case R.id.btnMissed:
+                    cursor = this.getContentResolver().query(CallLog.Calls.CONTENT_URI,
+                            null,
+                            CallLog.Calls.TYPE + " = " + CallLog.Calls.MISSED_TYPE,
+                            null,
+                            CallLog.Calls.DATE + " DESC");
+
+                    CursorToTable(cursor, tblMain);
+                    break;
+            }
+        } catch (SecurityException sec) {
+            Toast.makeText(this, "Permission missing: " + sec.getMessage(),
+                    Toast.LENGTH_SHORT).show();
+        } catch (Exception ex) {
+            Toast.makeText(this, "Error in MainActivity.onClick: " + ex.getMessage(),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void CursorToTable(Cursor cursor, TableLayout tbl) {
+        // Clear table if already populated
+        tbl.removeAllViews();
+
+        if (!cursor.moveToFirst())
+            // Empty cursor
+            return;
+
+        // Column Headers
+        TableRow headerRow = new TableRow(this);
+
+        for (int i = 0; i < cursor.getColumnCount(); i++) {
+            TextView textView = new TextView(this);
+
+            textView.setGravity(Gravity.CENTER_HORIZONTAL);
+            textView.setText(cursor.getColumnName(i));
+            textView.setPadding(0, 0, 5, 0);
+            textView.setAlpha(0.8f);
+
+            headerRow.addView(textView);
+        }
+
+        headerRow.setPadding(10, 10, 10, 10);
+
+        tbl.addView(headerRow);
+
+        // Rows
+
+        for (int i = 0; i < cursor.getCount(); i++) {
+            TableRow row = new TableRow(this);
+
+            for (int j = 0; j < cursor.getColumnCount(); j++) {
+                TextView textView = new TextView(this);
+
+                textView.setGravity(Gravity.CENTER_HORIZONTAL);
+                textView.setText(cursor.getString(j));
+                textView.setPadding(0, 0, 5, 0);
+
+                row.addView(textView);
+            }
+
+            row.setPadding(10, 10, 10, 10);
+
+            tbl.addView(row);
+
+            cursor.moveToNext();
+        }
+
+        cursor.close();
+
+    }
+}
